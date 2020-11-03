eb9fde0809a66cde23367322721fcf1c796334e8
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2020-11-03 16:06:00 +0800

    Update reinforcement_learning.py

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,92 +1,104 @@
 import numpy as np
 import random
+status=0
+ACTIONS = ["l", "r"]
 
-ALPHA = 0.1
 GAMMA = 0.9
 EPSILON = 0.9
 EPOCH = 20
 N_STATE = 6
-ACTIONS = ["left", "right"]
+ALPHA = 0.1
 
-np.random.seed(2)
+q_table=np.zeros((6,2))
 
-Q_Table = np.zeros((N_STATE, len(ACTIONS)))
 
+
+def get_value(S, A):
+    if A == 'l':
+        return q_table[S, 0]
+    else:
+        return q_table[S, 1]
+
+def take_action(status):
+    maxq=np.max(q_table[status])
+    
+        
+    if status==0:
+        action='r'
+        
+    else:
+        if maxq == 0 or np.random.uniform() > 0.9:
+            action = np.random.choice(ACTIONS)
+        else:
+            action='l' if np.argmax(q_table[status])==0 else 'r'
+    n_status=status+1 if action=='r' else status-1
+ 
+    maxq=np.max(q_table[n_status])
+    
+    return action,n_status,maxq
 
 def get_reward(S, A):
     if S == 5:
         R = 1
-    elif S == 4 and A == 'right':  # S!=5
+    elif S == 4 and A == 'r':  # S!=5
         R = 1
     else:
         R = 0
     return R
-
-
-def take_action(S):
-    if S == 0:
-        return 1, 'right', np.max(Q_Table[1, :])
-    maxQ = np.max(Q_Table[S, :])
-    if maxQ == 0 or np.random.uniform() > EPSILON:
-        if maxQ == 0:
-            print("maxQ is 0")
-        else:
-            print("Random 0.1 hit")
-        action = np.random.choice(ACTIONS)
-    else:
-        idx = np.argmax(Q_Table[S, :])
-        print("IDX:", idx)
-        action = ACTIONS[idx]
-    if action == 'left':
-        SN = S - 1
-    else:
-        SN = S + 1
-    maxQ = np.max(Q_Table[SN, :])
-    return SN, action, maxQ
-
-
-def get_value(S, A):
-    if A == 'left':
-        return Q_Table[S, 0]
-    else:
-        return Q_Table[S, 1]
-
-
-def set_value(S, A, V):
-    if A == 'left':
-        Q_Table[S, 0] = V
-    else:
-        Q_Table[S, 1] = V
-
-
 def update_q(S, A, MQ):
-    value = (1 - ALPHA) * get_value(S, A) + ALPHA * (get_reward(S, A) + GAMMA * MQ)
+    value = (1 - ALPHA) * get_value(s, A) + ALPHA * (get_reward(S, A) + GAMMA * MQ)
     set_value(S, A, value)
 
-EPOCH=20
-for loop in range(EPOCH):
-    print("Loop:", loop)
-    S = 0
-    step = 0
-    steps = []
-    while S != 5:
+def set_value(S, A, V):
+    if A == 'l':
+        q_table[S, 0] = V
+    else:
+        q_table[S, 1] = V
+
+
+for epoch in range(500):
+    status=0
+    action_set=[]
+    while status!=5:
         
-        SN, action, maxQ = take_action(S)
-        update_q(S, action, maxQ)
-        S = SN
-        step += 1
-        steps.append(action)
-    print("State:", S, "TotalSteps:", step, "\nDetail:", steps)
-
-print("Q_Table:", Q_Table)
-
+        action,n_status,maxq=take_action(status)
+        
+        value = (1 - ALPHA) * get_value(status, action) + ALPHA * (get_reward(status, action) + GAMMA * maxq)
+   
+        set_value(status, action, value)
+        status=n_status
+        action_set.append(action)
+        
+       
+   
+    # print(action_set) 
+   
 S = random.randint(0, 5)
-print("Initial S", S)
-step = 0
-steps = []
+print(S)
+step_set=[]   
 while S != 5:
-    SN, action, maxQ = take_action(S)
-    S = SN
-    step += 1
-    steps.append(action)
-print("Real State:", S, "TotalSteps:", step, "\nDetail:", steps)
+    action, n_status,maxQ = take_action(S)
+    print(n_status)
+    S=n_status
+    step_set.append(action)   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
