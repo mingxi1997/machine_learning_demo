300d2deaf642eacb5954c954bfffb150ba717614
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2021-01-29 13:47:21 +0800

    Update policy_gradient_different.py

--- policy_gradient_different.py
+++ policy_gradient_different.py
@@ -20,6 +20,9 @@ class NN(nn.Module):
         self.fc1= nn.Linear(4, 36)
         # self.fc2= nn.Linear(36, 36)
         self.fc3= nn.Linear(36, 2)
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform(m.weight)        
     def forward(self,x):
         out=torch.relu(self.fc1(x))
         # out=self.fc2(out)
