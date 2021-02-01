67f46b5317afc8df8f70ca866ed19454ef61e9c1
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2021-02-01 09:38:09 +0800

    Update policy_gradient_different.py

--- policy_gradient_different.py
+++ policy_gradient_different.py
@@ -18,14 +18,17 @@ class NN(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1= nn.Linear(4, 36)
-        
-        self.fc2= nn.Linear(36, 2)
+        # self.fc2= nn.Linear(36, 36)
+        self.fc3= nn.Linear(36, 2)
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
+                nn.init.xavier_uniform_(m.weight)        
     def forward(self,x):
         out=torch.relu(self.fc1(x))
-        out=self.fc2(out)
+        # out=self.fc2(out)
+        out=self.fc3(out)
+        
+        
         out=torch.nn.Softmax(dim=0)(out)
         return out
    
@@ -44,9 +47,10 @@ def gen_reward(reward):
      return n_reward
 
 
-device=torch.device('cuda:0')
      
-model=NN().to(device)
+model=NN()       
+        
+
 env = gym.make('CartPole-v0')
 env._max_episode_steps = 500
 status = env.reset()
@@ -57,42 +61,53 @@ action_set=[]
 reward_set=[]
 status_set=[]
 discount=0.9
-def check_count():
-    model.eval()
 
-
+print('collecting data')
+for i in tqdm(range(100)):
     status = env.reset()  
-     
+    raw_reward_set=[]
     done=False
-    count=0
     while not done:
-        count+=1
-        
-        
-        x=torch.tensor(status).to(torch.float32).to(device)
-        y=model(x)
-        
-        a=torch.distributions.Categorical(y)
-        
-        action=int(a.sample().item())
-
-        status,reward,done,_=env.step(action)
-        
-print('count',count)
-class loss_set:
-    def __init__(self):
-        self.sum=0
-        self.n=0
-    def add(self,num):
-        self.sum+=num
-        self.n+=1
-    def show(self):
-        out=self.sum/self.n
-        self.sum=0
-        self.n=0
-        return out
+        status_set.append(status)
     
-mloss=loss_set()
+    
+    
+        count+=1
+        # env.render()
+        action=strategy_raw(status)
+    
+        action_set.append(action)
+        status,reward,done,_=env.step(action)
+    
+    # if done:
+    #     reward=0
+    
+        raw_reward_set.append(reward)
+    
+    # print(model(torch.tensor(status).to(torch.float32)))
+    raw_reward_set=gen_reward(raw_reward_set)
+    reward_set.extend(raw_reward_set)
+device=torch.device('cuda:0')
+
+actions=torch.tensor(action_set).to(torch.float32).to(device)
+
+rewards=np.array(reward_set)
+# rewards=rewards-sum(rewards)/len(rewards)
+
+reward_mean = np.mean(rewards)
+reward_std = np.std(rewards)
+rewards= (rewards - reward_mean) / reward_std
+
+
+
+rewards=torch.tensor(rewards).to(torch.float32).to(device)
+
+statuses=torch.tensor(status_set)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+model=model.to(device)
 
 def test_count():
   model.eval()
@@ -113,95 +128,50 @@ def test_count():
         
         action=int(m.sample().item())
 
-        #print(action)
+       
         # action=strategy_raw(status)
     
         status,reward,done,_=env.step(action)
         
   print('count',count)
-
-def strategy_nn(status):
-    x=torch.tensor(status).to(torch.float32).to(device)
-    y=model(x)
-        
-    a=torch.distributions.Categorical(y)
-        
-    action=int(a.sample().item())
-    return action
+class loss_set:
+    def __init__(self):
+        self.sum=0
+        self.n=0
+    def add(self,num):
+        self.sum+=num
+        self.n+=1
+    def show(self):
+        out=self.sum/self.n
+        self.sum=0
+        self.n=0
+        return out
     
-
-
-for s in range(1000):
-
- test_count()
-
- for i in range(1):
-    status = env.reset()  
-    raw_reward_set=[]
-    done=False
-    while not done:
-        status_set.append(status)    
-        count+=1
-        # env.render()
-        action=strategy_nn(status)
-    
-        action_set.append(action)
-        status,reward,done,_=env.step(action)
-    
-    # if done:
-    #     reward=0
-    
-        raw_reward_set.append(reward)
-    
-    # print(model(torch.tensor(status).to(torch.float32)))
-    raw_reward_set=gen_reward(raw_reward_set)
-    reward_set.extend(raw_reward_set)
-
- actions=torch.tensor(action_set).to(torch.float32).to(device)
-
- rewards=np.array(reward_set)
-# rewards=rewards-sum(rewards)/len(rewards)
-
- reward_mean = np.mean(rewards)
- reward_std = np.std(rewards)
- rewards= (rewards - reward_mean) / reward_std
+mloss=loss_set()
 
 
 
- rewards=torch.tensor(rewards).to(torch.float32).to(device)
+num_epochs=200
 
- statuses=torch.tensor(status_set)
-
- optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-
-# model=model.to(device)
-
-
-
-
-
-
- num_epochs=1
-
- for epoch in range(num_epochs):
+for epoch in range(num_epochs):
+  test_count()
+  model.train()
   for i in range(len(actions)):
     optimizer.zero_grad()
+    
+    
     prob=model(statuses[i].to(torch.float32).to(device))
-    # print(prob.item())
-    a=torch.distributions.Categorical(prob)
-    loss=-a.log_prob(actions[i])*rewards[i]
+    
+    m=torch.distributions.Categorical(prob)
+   
+   
+    loss=-torch.pow(np.e,m.log_prob(actions[i]))*rewards[i]/0.5
+    
+    # loss=m.log_prob(actions[i])*rewards[i]
+    
     mloss.add(loss.item())
     loss.backward()
     optimizer.step()
-    # if (i+1) % 200 == 0:
-    #         print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-    #                 .format(epoch+1, num_epochs, i+1, len(actions), mloss.show()))
-# rewards=torch.tensor(rewards)
-
-
-
-
 
 
 
