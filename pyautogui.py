6c04f48ec213db4153b16a23b258d145f08ba1f6
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2021-05-25 11:30:26 +0800

    Create pyautogui.py

---
+++ pyautogui.py
@@ -0,0 +1,9 @@
+
+import pyautogui
+# print(pyautogui.position())
+def start_drive():
+    pyautogui.leftClick(74, 890)
+    pyautogui.leftClick(566,512)
+    pyautogui.leftClick(501, 404)
+    
+start_drive()
